185cf12d98431ac8991eca622fca617f7c7107ef
Lloople <hello@davidllop.com>
2019-04-16 21:13:40 +0200

    Use single quotes

--- src/config/snake.js
+++ src/config/snake.js
@@ -7,5 +7,5 @@ export default Object.freeze({
     DIRECTION_LEFT: 'left',
     DIRECTION_RIGHT: 'right',
     HEAD_START: '3,8',
-    BODY_START: ["3,7", "3,6", "3,5", "4,5", "5,5"],
+    BODY_START: ['3,7', '3,6', '3,5', '4,5', '5,5'],
 });
