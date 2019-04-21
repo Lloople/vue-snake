3d440822e4f9a0541e2c2b0135f5f3223c121f0a
Lloople <d.lloople@icloud.com>
2019-04-21 14:19:39 +0200

    Format more files using PHPStorm

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,32 +1,32 @@
 <template>
-	<div :data-coords='coords' class="square" :class="contentClass"></div>
+    <div :data-coords='coords' class="square" :class="contentClass"></div>
 </template>
 
 <script>
-import SNAKE from "./../config/snake.js";
+    import SNAKE from "./../config/snake.js";
 
-export default {
-	name: "Square",
-	props: {
-		content: Number,
-		coords: String
-	},
-	computed: {
-		contentClass() {
-			if (this.content === SNAKE.BODY) {
-				return 'body';
-			}
+    export default {
+        name: "Square",
+        props: {
+            content: Number,
+            coords: String
+        },
+        computed: {
+            contentClass() {
+                if (this.content === SNAKE.BODY) {
+                    return 'body';
+                }
 
-			if (this.content === SNAKE.HEAD) {
-				return 'head';
-			}
+                if (this.content === SNAKE.HEAD) {
+                    return 'head';
+                }
 
-			if (this.content === SNAKE.FOOD) {
-				return 'food';
-			}
+                if (this.content === SNAKE.FOOD) {
+                    return 'food';
+                }
 
-			return null;
-		}
-	}
-};
+                return null;
+            }
+        }
+    };
 </script>
